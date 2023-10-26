c097fccbb5abc792192fa407ad55c0723072a366
Maria Vitória <sobrinhomariavitoria@gmail.com>
2023-10-26 18:25:34 -0300

    pendencia

--- Avaliacao_I/classes_avaliacao01_esiii/q06/Teste.java
+++ Avaliacao_I/classes_avaliacao01_esiii/q06/Teste.java
@@ -4,10 +4,10 @@ public class Teste {
     public static void main(String[] args) {
         Produto produto = new Produto("1", "Celular", 2000, 0.5);
         Servico servico = new Servico("2", "Concerto", 200, 5);
-        Doacao doacao = new Doacao("2", "doacao", 50, 0.1);
+        Doacao doacao = new Doacao("3", "Doacao", 50, 0.1);
 
         Totalizacao totalizacao = new Totalizacao();
-        totalizacao.totalizar([produto, servico, doacao;]);
+        totalizacao.totalizar((Totalizavel[]) new Item[]{produto, servico, doacao});
 
         System.out.println(totalizacao);
     }
