4c08fe4ce7dac3a75c36e06390df93619332ce45
Maria Vitória Sobrinho Goes e  Silva <110963488+maviiigoes@users.noreply.github.com>
2023-11-23 14:27:32 -0300

    Commit

---
+++ Atividade 4 pt 2/q5/Perfil.java
@@ -0,0 +1,21 @@
+package q5;
+
+public class Perfil {
+    private String nomeUsuario;
+
+    public Perfil(int id, String nomeUsuario) {
+        this.nomeUsuario = nomeUsuario;
+    }
+
+    // Outros métodos
+    public String getNomeUsuario() {
+        return nomeUsuario;
+    }
+}
+
+
+
+
+
+
+
